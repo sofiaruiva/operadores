c4fab13f1ab40e501cfc4bedcfce3b13bd0d1868
sofiaruiva <134504062+sofiaruiva@users.noreply.github.com>
2023-09-20 07:57:30 -0300

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,15 @@
+function somavalores(){
+    var s1 = document.getElementById('valor1').value;
+    var s2 = document.getElementById('valor2').value;
+    var s3 =parseInt(s1) + parseInt (s2)
+ 
+    alert(s3)
+}
+    function subtraivalores(){
+        var s1 = document.getElementById('valor1').value;
+        var s2 = document.getElementById('valor2').value;
+        var s3 =parseInt(s1) - parseInt (s2)
+     
+        alert(s3)
+
+}
